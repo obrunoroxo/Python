071b977a89f3c6e1fdd0c6bc78d1d64a953bb352
Rossi <bruno.rossi22@outlook.com>
2023-11-19 03:35:55 -0300

    [ ADD ] - Adding a new ETL project.

---
+++ ETL/main.py
@@ -0,0 +1,77 @@
+import requests
+import typing as T
+
+from src.core.interfaces.interfaces import BaseHardCode
+
+class HardCode(BaseHardCode):
+    def __init__(
+        self,
+        url: str | None = None,
+        version: str | None = None,
+        **kwargs
+    ) -> None:
+        self.req = requests
+        self.url = url or 'https://restcountries.com'
+        self.version = version or 'v3.1'
+        self.endpoint: T.Optional[T.Union[str, None]] = f'{self.url}/{self.version}/' if self.url and self.version else None
+        self.response_content = set()
+        self.response_keys: list = [
+            "tld",
+            "cca2",
+            "ccn3",
+            "cca3",
+            "cioc",
+            "idd"
+        ]
+
+        super().__init__(**kwargs)
+
+
+    @property
+    def check_status(self) -> int:
+        response = self.req.get(self.endpoint+'all')
+        response_status = response.status_code
+        if response_status == 200:
+            try:
+                response = response.json()
+            except Exception as err:
+                raise err
+            self.response_content = response
+        return response_status
+
+
+    def get_response_content(self) -> list:
+        information_key = []
+
+        if self.response_content and isinstance(self.response_content, list):
+            for item in self.response_content:
+                for key, _ in item.items():
+                    if not key in self.response_keys:
+                        information_key.append(key)
+                    else:
+                        continue
+                break
+
+        return information_key
+
+
+    def manipulating_content(self, information_key: list):
+        print(information_key)
+
+
+    # def start_process(self):
+    #     response_req = self.req.get(self.endpoint+'/all')
+    #     response_status = response_req.status_code
+    #     response_content = response_req.content
+    #     print(response_req)
+
+
+
+
+
+
+if __name__ == '__main__':
+    def main():
+        instance = HardCode()
+        instance.start_process()
+    main()
---
+++ ETL/src/core/interfaces/interfaces.py
@@ -0,0 +1,27 @@
+from abc import (
+    ABC,
+    abstractmethod,
+    abstractproperty,
+    abstractclassmethod,
+    abstractstaticmethod
+)
+
+
+class BaseHardCode(ABC):
+    @abstractmethod
+    @abstractproperty
+    def check_status(self) -> int:
+        pass
+
+    @abstractmethod
+    def get_response_content(self) -> list:
+        pass
+
+    @abstractmethod
+    def manipulating_content(self, information_key: list):
+        pass
+
+    def start_process(self):
+        print(self.check_status)
+        information_key = self.get_response_content()
+        self.manipulating_content(information_key = information_key)
